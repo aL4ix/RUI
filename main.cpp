b553cf8a186754761a19705ade779dc9afa3d854
fuumarumota <fed.gimeno@gmail.com>
2020-06-17 15:12:51 -0500

    wxWidgets multithread test

--- main.cpp
+++ main.cpp
@@ -1,49 +1,149 @@
-#if defined(UNICODE) && !defined(_UNICODE)
-    #define _UNICODE
-#elif defined(_UNICODE) && !defined(UNICODE)
-    #define UNICODE
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
 #endif
 
-#include <tchar.h>
-#include <windows.h>
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+#include <wx/thread.h>
 #include <SDL2/SDL.h>
-#include <iostream>
 
-/*  Declare Windows procedure  */
-LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
-/*  Make the class name into a global variable  */
-TCHAR szClassName[ ] = _T("RUIClass");
+/*******************************************************************
+    SDL THREAD THREAD CLASS
+*******************************************************************/
+// A thread class that will periodically send events to the GUI thread,
+// we use this as the SDL Events thread.
 
-SDL_Rect cube;
-SDL_Renderer* renderer = NULL;
-bool isCubeGoingRight = true;
-bool isCubeGoingDown = true;
+class MyThread : public wxThread
+{
+protected:
+   wxEvtHandler* m_parent;
 
-int Render(void *data) {
-    for(int i=0; i<1000; i++) {
-        //Animation
-        if(isCubeGoingRight) {
-            ++cube.x;
-        } else {
-            --cube.x;
+public:
+    MyThread(wxEvtHandler *parent) : wxThread(), m_parent(parent)
+    {
+    };
+
+   ExitCode Entry();
+};
+
+
+wxThread::ExitCode MyThread::Entry()
+{
+
+    wxThreadEvent *evt = new wxThreadEvent(wxEVT_THREAD);
+    m_parent->QueueEvent(evt);
+    return (wxThread::ExitCode) 0;
+}
+
+/********************************************************************
+    SDL PANEL CLASS
+*******************************************************************/
+
+class MyPanel : public wxPanel
+{
+public:
+    MyPanel(wxWindow *parent) : wxPanel (parent, wxID_ANY){
+
+        SDL_Init(SDL_INIT_EVERYTHING);
+        window = SDL_CreateWindowFrom(this->GetHandle());
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+
+        m_timer.SetOwner(this);
+        m_timer.Start(15);
+        thread = new MyThread( this );
+
+        cube.x = 0;
+        cube.y = 0;
+        cube.w = 20;
+        cube.h = 20;
+
+        if( thread->Create()==wxTHREAD_NO_ERROR )
+        {
+            thread->Run();
         }
-        if(isCubeGoingDown) {
-            ++cube.y;
-        } else {
-            --cube.y;
+    }
+    ~MyPanel(){
+        renderer = nullptr;
+        SDL_Quit();
+    }
+
+    MyThread *thread;
+    wxTimer m_timer;
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    SDL_Rect cube;
+
+    bool isCubeGoingRight = true;
+    bool isCubeGoingDown = true;
+
+    bool sdl_quit = false;
+    void OnThreadEvent(wxThreadEvent &event);
+    void OnTimer(wxTimerEvent &event);
+
+   DECLARE_EVENT_TABLE()
+};
+
+BEGIN_EVENT_TABLE(MyPanel, wxPanel)
+    EVT_THREAD(wxID_ANY, MyPanel::OnThreadEvent)
+    EVT_TIMER(-1,MyPanel::OnTimer)
+END_EVENT_TABLE()
+
+
+void MyPanel::OnThreadEvent(wxThreadEvent &event)
+{
+
+    SDL_Event evt;
+	while (!sdl_quit){
+        while (SDL_PollEvent(&evt)) {
+
+
+            if ( evt.type == SDL_KEYDOWN )
+            {
+                std::cout << "KeyPress" << std::endl;
+            }
         }
-        if(cube.x >= 500) {
-            isCubeGoingRight = false;
-        } else if(cube.x <= 0) {
-            isCubeGoingRight = true;
-        }
-        if(cube.y >= 300) {
-            isCubeGoingDown = false;
-        } else if(cube.y <= 0) {
-            isCubeGoingDown = true;
-        }
-    // Clear the window and make it all green
+
+        SDL_Delay(16);
+
+	}
+    std::cout << "Quit" << std::endl;
+
+}
+
+
+void MyPanel::OnTimer(wxTimerEvent &event)
+{
+    /*This event seems to run even if the GUI is waiting on events. I use this kind of a update->render thread, while
+    the input is handled by another thread.
+
+    You see the GUI thread not blocking the rendering routine. It fills you with determination. */
+
+    if(isCubeGoingRight) {
+        ++cube.x;
+    } else {
+        --cube.x;
+    }
+    if(isCubeGoingDown) {
+        ++cube.y;
+    } else {
+        --cube.y;
+    }
+    if(cube.x >= 500) {
+        isCubeGoingRight = false;
+    } else if(cube.x <= 0) {
+        isCubeGoingRight = true;
+    }
+    if(cube.y >= 300) {
+        isCubeGoingDown = false;
+    } else if(cube.y <= 0) {
+        isCubeGoingDown = true;
+    }
+
     SDL_RenderClear( renderer );
     // Change color to blue
     SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
@@ -53,179 +153,126 @@ int Render(void *data) {
     SDL_SetRenderDrawColor( renderer, 0, 255, 0, 255 );
     // Render the changes above
     SDL_RenderPresent( renderer);
-    SDL_Delay( 16 );
-    }
 
 }
 
-int WINAPI WinMain (HINSTANCE hThisInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR lpszArgument,
-                     int nCmdShow)
+/*******************************************************************
+    MAIN FRAME CLASS
+*******************************************************************/
+
+// Define a new frame type: this is going to be our main frame
+class MyFrame : public wxFrame
 {
-    HWND hwnd;               /* This is the handle for our window */
-    MSG messages;            /* Here messages to the application are saved */
-    WNDCLASSEX wincl;        /* Data structure for the windowclass */
+public:
+    MyFrame(const wxString& title);
+    void close();
+    void OnQuit(wxCommandEvent& event);
+    void OnAbout(wxCommandEvent& event);
+    void OnClose (wxCloseEvent &event);
 
-    /* The Window structure */
-    wincl.hInstance = hThisInstance;
-    wincl.lpszClassName = szClassName;
-    wincl.lpfnWndProc = WindowProcedure;      /* This function is called by windows */
-    wincl.style = CS_HREDRAW | CS_VREDRAW;                 /* Catch double-clicks */
-    wincl.cbSize = sizeof (WNDCLASSEX);
+private:
+		wxStaticText *m_st;
+		MyPanel *m_pan;
 
-    /* Use default icon and mouse-pointer */
-    wincl.hIcon = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hIconSm = LoadIcon (NULL, IDI_APPLICATION);
-    wincl.hCursor = LoadCursor (NULL, IDC_ARROW);
-    wincl.lpszMenuName = NULL;                 /* No menu */
-    wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
-    wincl.cbWndExtra = 0;                      /* structure or the window instance */
-    /* Use Windows's default colour as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+    DECLARE_EVENT_TABLE()
+};
 
-    /* Register the window class, and if it fails quit the program */
-    if (!RegisterClassEx (&wincl))
-        return 0;
-
-    /* The class is registered, let's create the program*/
-    hwnd = CreateWindowEx (
-           0,                   /* Extended possibilites for variation */
-           szClassName,         /* Classname */
-           _T("RUI"),       /* Title Text */
-           WS_OVERLAPPEDWINDOW, /* default window */
-           CW_USEDEFAULT,       /* Windows decides the position */
-           CW_USEDEFAULT,       /* where the window ends up on the screen */
-           600,                 /* The programs width */
-           400,                 /* and height in pixels */
-           HWND_DESKTOP,        /* The window is a child-window to desktop */
-           NULL,                /* No menu */
-           hThisInstance,       /* Program Instance handler */
-           NULL                 /* No Window Creation data */
-           );
-    /* Make the window visible on the screen */
-    ShowWindow (hwnd, nCmdShow);
-
-    // WinAPI CODE: A menu example
-    HMENU hMenu, hSubMenu;
-    hMenu = CreateMenu();
-    hSubMenu = CreatePopupMenu();
-    AppendMenu(hSubMenu, MF_STRING, 9001, "E&xit");
-    AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
-    hSubMenu = CreatePopupMenu();
-    AppendMenu(hSubMenu, MF_STRING, 9002, "&Spam");
-    AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&Edit");
-    SetMenu(hwnd, hMenu);
-
-    /*
-    //Original message loop, this was replaced by an SDL loop
-    while (GetMessage (&messages, NULL, 0, 0))
-    {
-        // Translate virtual-key messages into character messages
-        TranslateMessage(&messages);
-        // Send message to WindowProcedure
-        DispatchMessage(&messages);
-    }
-    */
-
-    // FROM HERE IS NORMAL CODE
-    // Init everything
-    if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
-	{
-		std::cout << " Failed to initialize SDL: " << SDL_GetError() << std::endl;
-		return false;
-	}
-	SDL_Window* sdl_window = SDL_CreateWindowFrom(hwnd);
-	if (sdl_window == NULL) {
-        std::cerr << "SDL CreateWindowFrom ERROR: " << SDL_GetError() << std::endl;
-        return 0;
-    }
-    renderer = SDL_CreateRenderer(sdl_window, -1, 0);
-    if (renderer == NULL) {
-        std::cerr << "SDL: failed to create renderer: " << SDL_GetError();
-    }
-
-    // Init vars
-
-    cube.x = 0;
-	cube.y = 0;
-	cube.w = 20;
-	cube.h = 20;
-    bool loop = true;
-    //Stuff for the animation
-    SDL_Thread* threadID = SDL_CreateThread(Render, "Render", NULL);
-
-    //Our very own main loop
-	while ( loop )
-	{
-		SDL_Event event;
-		while ( SDL_PollEvent( &event ) )
-		{
-			if ( event.type == SDL_QUIT )
-				loop = false;
-			else if ( event.type == SDL_KEYDOWN )
-			{
-				switch ( event.key.keysym.sym )
-				{
-					case SDLK_RIGHT:
-						cube.x += 2;
-						break;
-					case SDLK_LEFT:
-						cube.x -= 2;
-						break;
-					case SDLK_DOWN:
-						cube.y += 2;
-						break;
-					case SDLK_UP:
-						cube.y -= 2;
-						break;
-					default :
-						break;
-				}
-			}
-		}
-
-
-
-        //Render();
-
-		// Add a 16msec delay to make our game run at ~60 fps
-		SDL_Delay( 16 );
-	}
-
-
-    /* The program return-value is 0 - The value that PostQuitMessage() gave */
-    return messages.wParam;
-}
-
-
-/*  This function is called by the Windows function DispatchMessage()  */
-
-LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+enum
 {
-    //std::cout << 'E' << message << ' ';
-    switch (message)                  /* handle the messages */
-    {
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
-        /*case WM_SYSCOMMAND:
-            // disallow screen saver while emulating (doesn't work if password protected)
-            if ((((wParam & 0xFFF0) == SC_SCREENSAVE) || ((wParam & 0xFFF0) == SC_MONITORPOWER)))
-                return 0;*/
-        /*case WM_PAINT:
-            {
-                PAINTSTRUCT ps;
-                HDC hdc = BeginPaint(hwnd, &ps);
-                //Render();
-                EndPaint(hwnd, &ps);
-            }
-            break;*/
-        default:                      /* for messages that we don't deal with */
-            LRESULT a = DefWindowProc (hwnd, message, wParam, lParam);
-            //std::cout << 'X' << message << std::endl;
-            return a;
-    }
+    // menu items
+    Minimal_Quit = wxID_EXIT,
+    Minimal_About = wxID_ABOUT
+};
 
-    return 0;
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+    EVT_MENU(Minimal_Quit,  MyFrame::OnQuit)
+    EVT_MENU(Minimal_About, MyFrame::OnAbout)
+    EVT_CLOSE(MyFrame::OnClose)
+END_EVENT_TABLE()
+
+
+MyFrame::MyFrame(const wxString& title)
+       : wxFrame(NULL, wxID_ANY, title)
+{
+
+    // set the frame icon
+    SetIcon(wxICON(sample));
+
+#if wxUSE_MENUS
+    // create a menu bar
+    wxMenu *fileMenu = new wxMenu;
+
+    // the "About" item should be in the help menu
+    wxMenu *helpMenu = new wxMenu;
+    helpMenu->Append(Minimal_About, "&About\tF1", "Show about dialog");
+
+    fileMenu->Append(Minimal_Quit, "E&xit\tAlt-X", "Quit this program");
+
+    // now append the freshly created menu to the menu bar...
+    wxMenuBar *menuBar = new wxMenuBar();
+    menuBar->Append(fileMenu, "&File");
+    menuBar->Append(helpMenu, "&Help");
+
+    // ... and attach this menu bar to the frame
+    SetMenuBar(menuBar);
+#endif // wxUSE_MENUS
+
+#if wxUSE_STATUSBAR
+    // create a status bar just for fun (by default with 1 pane only)
+    CreateStatusBar(2);
+    SetStatusText("Welcome to wxWidgets!");
+#endif // wxUSE_STATUSBAR
+
+    m_pan = new MyPanel(this);
+
 }
+
+// event handlers
+
+void MyFrame::close(){
+
+    m_pan->sdl_quit = true;
+    Destroy();
+
+}
+void MyFrame::OnQuit(wxCommandEvent &event)
+{
+    // true is to force the frame to close
+    Close(true);
+    close();
+}
+
+void MyFrame::OnClose(wxCloseEvent &event){
+    close();
+}
+
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+}
+
+/*******************************************************************
+    APPLICATION CLASS
+*******************************************************************/
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+// 'Main program' equivalent: the program execution "starts" here
+bool MyApp::OnInit()
+{
+
+    if ( !wxApp::OnInit() )
+        return false;
+
+    // create the main application window
+    MyFrame *frame = new MyFrame("Minimal wxWidgets App");
+    frame->SetSize(wxSize(600,400));
+    frame->Show(true);
+    SetTopWindow(frame);
+    return true;
+}
+
+IMPLEMENT_APP(MyApp);
